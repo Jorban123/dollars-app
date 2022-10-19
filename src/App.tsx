da6a829e456c47c975082aeecb75e8ea76c6fac4
Jorban <zazulin24118@yandex.ru>
2022-10-19 21:05:40 +0300

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Header from './components/header/Header'
+const App: React.FC = () => {
   return (
     <div className="App">
-
+      <Header/>
     </div>
   );
 }
---
+++ src/components/window/Window.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import classes from './myWindow.module.css';
+
+const Window = () => {
+    return (
+        <div className={classes.window}>
+
+        </div>
+    )
+}
+
+export default Window;
