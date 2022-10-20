efacdccc3afacb7e652434ca19af15ecdc91ca5f
Jorban <zazulin24118@yandex.ru>
2022-10-20 22:15:34 +0300

    finish taking current currencies

--- src/components/currency/Currency.tsx
+++ src/components/currency/Currency.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Table from '@mui/material/Table';
+import { InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
@@ -7,39 +8,52 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import styles from './Currency.module.css';
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import axios from 'axios';
 
 const Currency = () => {
+    const apiKey = 'Ca9ubiwg33JpoQwrPBNOlDC8iqMrWxTw'
+    const [defaultCur, setDefaultCur] = React.useState('RUB')
+    const [currencies, setCurrencies] = React.useState({})
+    const handleChange = (e: SelectChangeEvent) => {
+        setDefaultCur(e.target.value)
+    }
+    React.useEffect(() => {
+        axios.get(`https://api.apilayer.com/exchangerates_data/latest?symbols=RUB,USD,KZT&base=${defaultCur}`, {
+            headers: {
+                apiKey
+            }
+        }).then(res => {
+            console.log(res.data.rates);
+            setCurrencies(res.data.rates)
+        })
+        Object.keys(currencies).map((key: string) => {
+            console.log(currencies[key as keyof typeof currencies])
+        })
+    }, [defaultCur])
+
+
   return (
     <TableContainer component={Paper} className={styles.container}>
+        <div className={styles.defaultCur}>
+            <InputLabel id="demo-simple-select-label">Базовая валюта</InputLabel>
+            <Select
+                value={defaultCur}
+                onChange={handleChange}
+            >
+                <MenuItem value={'RUB'}>RUB</MenuItem>
+                <MenuItem value={'USD'}>USD</MenuItem>
+                <MenuItem value={'EUR'}>EURO</MenuItem>
+            </Select>
+        </div>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell>Валюта</TableCell>
             <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {/* {rows.map((row) => (
             <TableRow
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -51,7 +65,20 @@ const Currency = () => {
               <TableCell align="right">{row.fat}</TableCell>
               <TableCell align="right">{row.carbs}</TableCell>
             </TableRow>
-          ))}
+          ))} */}
+          {Object.keys(currencies).map((key: string) => {
+            return (
+            <TableRow
+                key={key}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+                <TableCell component="th" scope="row">
+                    {key}
+                </TableCell>
+                <TableCell align="right">{currencies[key as keyof typeof currencies]}</TableCell>
+            </TableRow>
+            )
+          })}
         </TableBody>
       </Table>
     </TableContainer>
